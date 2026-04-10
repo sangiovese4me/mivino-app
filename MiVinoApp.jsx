9caedfd844ee62c63410539dc056397bf21de9a1
sangiovese4me <sangiovese4me@gmail.com>
2026-04-09 20:56:54 -0700

    Update MiVinoApp.jsx

--- MiVinoApp.jsx
+++ MiVinoApp.jsx
@@ -1,5 +1,3 @@
-“use client”;
-
 import React, { useState, useEffect, useRef, useCallback } from ‘react’;
 import { Trash2, ChevronDown, ChevronUp, Loader, Camera, X, ZoomIn } from ‘lucide-react’;
 
