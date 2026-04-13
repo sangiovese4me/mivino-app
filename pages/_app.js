3d5872babb735bdca4d7f9715d09c66f4f7341af
sangiovese4me <sangiovese4me@gmail.com>
2026-04-12 21:05:00 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import { ClerkProvider } from '@clerk/nextjs';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="theme-color" content="#5c1a2e" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="MiVino" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <Component {...pageProps} />
     </ClerkProvider>
   );
